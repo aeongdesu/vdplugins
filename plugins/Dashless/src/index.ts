472fc9d1ad72ef684d10b3ed835dfe249a887355
aeongdesu <osu@kakao.com>
2023-03-20 01:36:51 +0900

    i need it

--- plugins/Dashless/src/index.ts
+++ plugins/Dashless/src/index.ts
@@ -7,11 +7,12 @@ const { View } = ReactNative
 const unpatch = after("render", View, (_, res) => {
     const textChannel = findInReactTree(res, r => r?.props?.channel?.name && r?.props?.hasOwnProperty?.("isRulesChannel"))
     if (!textChannel) return
-    after("type", textChannel.type, (_, res: any) => {
+    const patch = after("type", textChannel.type, (_, res: any) => {
         const textChannelName = findInReactTree(res, r => typeof r?.children === "string")
         
         if (!textChannelName) return
         textChannelName.children = textChannelName.children.replace(/-/g, " ")
+        patch()
     })
 })
 export const onUnload = () => unpatch()
