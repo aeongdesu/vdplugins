45a099251ba8406b82d0c0861a9e2a422af6facb
aeongdesu <osu@kakao.com>
2023-03-26 11:15:45 +0900

    fix viewraw :trolley:

--- plugins/ViewRaw/src/index.tsx
+++ plugins/ViewRaw/src/index.tsx
@@ -1,53 +1,53 @@
 import { before, after } from "@vendetta/patcher"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
-import { findByProps as getByProps } from "@vendetta/metro"
+import { findByProps as getByProps, findByDisplayName } from "@vendetta/metro"
+import { Forms } from "@vendetta/ui/components"
 import RawPage from "./RawPage"
 
 export let message: any
 
-const ActionSheet = getByProps("hideActionSheet")
+const ActionSheet = getByProps("openLazy", "hideActionSheet")
 const Navigation = getByProps("push", "pushLazy", "pop")
 const DiscordNavigator = getByProps("getRenderCloseButton")
 const { default: Navigator, getRenderCloseButton } = DiscordNavigator
+const Icon = findByDisplayName("Icon")
+const { FormRow } = Forms
 
 const unpatch = before("openLazy", ActionSheet, (ctx) => {
     const [component, args, actionMessage] = ctx
-    if (args == "MessageLongPressActionSheet")
-        component.then(instance => {
-            const unpatch = after("default", instance, (_, component) => {
-                const [msgProps, oldbuttons] = component.props?.children?.props?.children?.props?.children
-                if (!msgProps) message = actionMessage.message
-                else message = msgProps.props.message
-                if (oldbuttons) {
-                    const MarkUnreadIndex = oldbuttons.findIndex((a: { props: { message: string } }) => a.props.message == "Copy Message Link")
-                    const ButtonRow = oldbuttons[MarkUnreadIndex].type
-                    const navigator = () => (
-                        <Navigator
-                            initialRouteName="RawPage"
-                            goBackOnBackPress={true}
-                            screens={{
-                                RawPage: {
-                                    title: "ViewRaw",
-                                    headerLeft: getRenderCloseButton(() => Navigation.pop()),
-                                    render: RawPage
-                                }
-                            }}
-                        />
-                    )
-                    component.props.children.props.children.props.children[1] = [...oldbuttons,
-                    <ButtonRow
-                        key={-1}
-                        message="View Raw"
-                        iconSource={getAssetId("ic_chat_bubble_16px")}
-                        onPressRow={() => {
-                            ActionSheet.hideActionSheet()
-                            Navigation.push(navigator)
-                        }}
-                    />]
-                }
-                unpatch()
-            })
+    if (args != "MessageLongPressActionSheet") return
+    component.then(instance => {
+        const unpatch = after("default", instance, (_, component) => {
+            const [msgProps, oldbuttons] = component.props?.children?.props?.children?.props?.children
+            if (!msgProps) message = actionMessage.message
+            else message = msgProps.props.message
+            if (!oldbuttons) return
+            const navigator = () => (
+                <Navigator
+                    initialRouteName="RawPage"
+                    goBackOnBackPress={true}
+                    screens={{
+                        RawPage: {
+                            title: "ViewRaw",
+                            headerLeft: getRenderCloseButton(() => Navigation.pop()),
+                            render: RawPage
+                        }
+                    }}
+                />
+            )
+            
+            component.props.children.props.children.props.children[1] = [...oldbuttons,
+            <FormRow
+                label="View Raw"
+                leading={<Icon source={getAssetId("ic_chat_bubble_16px")} />}
+                onPress={() => {
+                    ActionSheet.hideActionSheet()
+                    Navigation.push(navigator)
+                }}
+            />]
+            unpatch()
         })
+    })
 })
 
 export const onUnload = () => unpatch()
