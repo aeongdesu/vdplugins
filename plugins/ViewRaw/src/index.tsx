2a14651b3e3768d859876a76f7853cb60603696b
aeongdesu <osu@kakao.com>
2023-03-28 17:23:05 +0900

    will crash

--- plugins/ViewRaw/src/index.tsx
+++ plugins/ViewRaw/src/index.tsx
@@ -1,15 +1,14 @@
 import { before, after } from "@vendetta/patcher"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
 import { findByProps as getByProps, findByName } from "@vendetta/metro"
+import { NavigationNative } from "@vendetta/metro/common"
 import { Forms } from "@vendetta/ui/components"
 import RawPage from "./RawPage"
 
 export let message: any
 
 const ActionSheet = getByProps("openLazy", "hideActionSheet")
-const Navigation = getByProps("push", "pushLazy", "pop")
-const DiscordNavigator = getByProps("getRenderCloseButton")
-const { default: Navigator, getRenderCloseButton } = DiscordNavigator
+const navigation = NavigationNative.useNavigation()
 const Icon = findByName("Icon")
 const { FormRow } = Forms
 
@@ -22,19 +21,6 @@ const unpatch = before("openLazy", ActionSheet, (ctx) => {
             if (!msgProps) message = actionMessage.message
             else message = msgProps.props.message
             if (!oldbuttons) return
-            const navigator = () => (
-                <Navigator
-                    initialRouteName="RawPage"
-                    goBackOnBackPress={true}
-                    screens={{
-                        RawPage: {
-                            title: "ViewRaw",
-                            headerLeft: getRenderCloseButton(() => Navigation.pop()),
-                            render: RawPage
-                        }
-                    }}
-                />
-            )
             
             component.props.children.props.children.props.children[1] = [...oldbuttons,
             <FormRow
@@ -42,7 +28,10 @@ const unpatch = before("openLazy", ActionSheet, (ctx) => {
                 leading={<Icon source={getAssetId("ic_chat_bubble_16px")} />}
                 onPress={() => {
                     ActionSheet.hideActionSheet()
-                    Navigation.push(navigator)
+                    navigation.push("RawPage", {
+                        title: "ViewRaw",
+                        render: RawPage
+                    })
                 }}
             />]
             unpatch()
