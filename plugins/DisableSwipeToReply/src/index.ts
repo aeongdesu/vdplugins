3ec3b02d95fcff8150646ffc9b17c097b0921010
aeongdesu <osu@kakao.com>
2023-04-01 16:09:33 +0900

    more faster

--- plugins/DisableSwipeToReply/src/index.ts
+++ plugins/DisableSwipeToReply/src/index.ts
@@ -3,16 +3,8 @@ import { ReactNative } from "@vendetta/metro/common"
 
 const chatManager = ReactNative.NativeModules.DCDChatManager
 
-const unpatch = before("updateRows", chatManager, (args: any) => {
-    const messages = JSON.parse(args[1])
-    let newMessages = []
-    for (let i = 0; i < messages.length; i++) {
-        const data = messages[i]
-        if (data.enableSwipeToReply === undefined || data.enableSwipeToReply === false) continue
-        data.enableSwipeToReply = false
-        newMessages.push(messages[i])
-        args[1] = JSON.stringify(newMessages)
-    }
-})
+const regex = /(?<=enableSwipeToReply":)(true)/g
+
+const unpatch = before("updateRows", chatManager, args => args[1] = args[1].replace(regex, "false"))
 
 export const onUnload = () => unpatch()
