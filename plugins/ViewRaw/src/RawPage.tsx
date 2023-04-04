70982972cfdf0242ebebd25e561f6bca04cd4619
Vendicated <vendicated@riseup.net>
2023-04-04 23:57:21 +0200

    no more self dox

--- plugins/ViewRaw/src/RawPage.tsx
+++ plugins/ViewRaw/src/RawPage.tsx
@@ -1,16 +1,20 @@
-import { findByProps as getByProps } from "@vendetta/metro"
-import { ReactNative, constants as Constants, clipboard } from "@vendetta/metro/common"
+import { find, findByProps as getByProps } from "@vendetta/metro"
+import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common"
 import { showToast } from "@vendetta/ui/toasts"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
 
-import { message } from "./index"
+import { cleanMessage } from "./cleanMessage"
 
 const { ScrollView, Text, TextInput, Platform } = ReactNative
 const { OS } = Platform
 const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any
+const Parser = find(m => m.defaultRules?.codeBlock)
 
-export default function RawPage() {
-    const stringMessage = JSON.stringify(message, null, 4)
+console.log(Parser)
+
+export default function RawPage({ message }) {
+    const stringMessage = React.useMemo(() => JSON.stringify(cleanMessage(message), null, 4), [message.id])
+
     return (<>
         <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
             <Button
