13970748029383eddef599e5a3b00b69c8984e20
Vendicated <vendicated@riseup.net>
2023-04-05 00:27:01 +0200

    Add proper theme support

--- plugins/ViewRaw/src/RawPage.tsx
+++ plugins/ViewRaw/src/RawPage.tsx
@@ -1,16 +1,26 @@
-import { find, findByProps as getByProps } from "@vendetta/metro"
+import { findByProps as getByProps } from "@vendetta/metro"
 import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common"
 import { showToast } from "@vendetta/ui/toasts"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
-
 import { cleanMessage } from "./cleanMessage"
+import { stylesheet } from "@vendetta/metro/common";
+import { semanticColors } from "@vendetta/ui";
 
 const { ScrollView, Text, TextInput, Platform } = ReactNative
 const { OS } = Platform
 const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any
-const Parser = find(m => m.defaultRules?.codeBlock)
 
-console.log(Parser)
+const styles = stylesheet.createThemedStyleSheet({
+    codeBlock: {
+        fontFamily: Constants.Fonts.CODE_SEMIBOLD,
+        fontSize: 12,
+        backgroundColor: semanticColors.BACKGROUND_SECONDARY,
+        color: semanticColors.TEXT_NORMAL,
+        marginTop: 10,
+        borderRadius: 3,
+        padding: 10,
+    }
+})
 
 export default function RawPage({ message }) {
     const stringMessage = React.useMemo(() => JSON.stringify(cleanMessage(message), null, 4), [message.id])
@@ -28,12 +38,12 @@ export default function RawPage({ message }) {
             />
             {(OS == "ios")
                 ? <TextInput
-                    style={{ fontFamily: Constants.Fonts.CODE_SEMIBOLD, fontSize: 12, backgroundColor: "#282b30", color: "white", marginTop: 10, borderRadius: 3, padding: 10 }}
+                    style={styles.codeBlock}
                     onChange={() => { }}
                     multiline
                     value={stringMessage}
                 />
-                : <Text selectable style={{ fontFamily: Constants.Fonts.CODE_SEMIBOLD, fontSize: 12, backgroundColor: "#282b30", color: "white", marginTop: 10, borderRadius: 3, padding: 10 }}>
+                : <Text selectable style={styles.codeBlock}>
                     {stringMessage}
                 </Text>}
         </ScrollView>
