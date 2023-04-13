f20aff1fa93dc9f75388c075cef7f34a01bd5f3c
aeongdesu <osu@kakao.com>
2023-04-14 01:16:24 +0900

    MarkAllAsRead

--- plugins/MarkAllAsRead/src/index.ts
+++ plugins/MarkAllAsRead/src/index.ts
@@ -1,7 +1,7 @@
 import { findByStoreName, findByProps } from "@vendetta/metro"
-import { FluxDispatcher } from "@vendetta/metro/common"
 import { registerCommand } from "@vendetta/commands"
 import { logger } from "@vendetta"
+import { storage } from "@vendetta/plugin"
 
 const ClydeUtils = findByProps("sendBotMessage")
 const GuildStore = findByStoreName("GuildStore")
@@ -13,36 +13,38 @@ const unregisterCommand = registerCommand({
     displayName: "markallasread",
     description: "read all server notifications",
     displayDescription: "read all server notifications",
-    // @ts-ignore
-    applicationId: -1,
+    options: [],
+    applicationId: "",
     inputType: 1,
     type: 1,
 
     execute: async (args, ctx) => {
         try {
+            if (new Date() < storage.timeout) return ClydeUtils.sendBotMessage(ctx.channel.id, "Please wait 10 seconds!")
             const channels: Array<any> = []
 
             Object.values(GuildStore.getGuilds()).forEach((guild: any) => {
-                GuildChannelStore.getChannels(guild.id).SELECTABLE.forEach((c: { channel: { id: string } }) => {
-                    if (!ReadStateStore.hasUnread(c.channel.id)) return
+                if (!findByStoreName("UserGuildSettingsStore").isMuted(guild.id))
+                    GuildChannelStore.getChannels(guild.id).SELECTABLE.forEach((c: { channel: { id: string } }) => {
+                        if (!ReadStateStore.hasUnread(c.channel.id)) return
 
-                    channels.push({
-                        channel_id: c.channel.id,
-                        message_id: ReadStateStore.lastMessageId(c.channel.id)
+                        channels.push({
+                            channel_id: c.channel.id,
+                            message_id: ReadStateStore.lastMessageId(c.channel.id)
+                        })
                     })
-                })
             })
-
             await findByProps("setRequestPatch").default.post({
                 url: findByProps("Endpoints").Endpoints.BULK_ACK,
                 body: {
                     "read_states": channels.slice(0, 100)
                 }
             })
-            ClydeUtils.sendBotMessage(ctx.channel.id, "Done!")
+            storage.timeout = +new Date() + 10000
+            return ClydeUtils.sendBotMessage(ctx.channel.id, "Done!")
         } catch (e) {
             logger.error(e)
-            ClydeUtils.sendBotMessage(ctx.channel.id, "Failed to read all server notifications")
+            ClydeUtils.sendBotMessage(ctx.channel.id, "Failed to read all server notifications!")
         }
     }
 })
