0cc95404c821ffafd07f2e10aed0c36f01cf602e
aeongdesu <osu@kakao.com>
2023-04-16 14:21:58 +0900

    fix userbg

---
+++ plugins/UserBG/src/Settings.tsx
@@ -0,0 +1,29 @@
+import { React, url } from "@vendetta/metro/common"
+import { getAssetIDByName } from "@vendetta/ui/assets"
+import { Forms, General } from "@vendetta/ui/components"
+import { showToast } from "@vendetta/ui/toasts"
+
+import { fetchDB } from "./fetchDB"
+
+const { ScrollView } = General
+const { FormSection, FormRow } = Forms
+
+export default () => (<ScrollView>
+    <FormSection>
+        <FormRow
+            label="Discord Server"
+            leading={<FormRow.Icon source={getAssetIDByName("Discord")} />}
+            trailing={FormRow.Arrow}
+            onPress={() => url.openDeeplink("https://discord.gg/TeRQEPb")}
+        />
+        <FormRow
+            label="Reload DB"
+            leading={<FormRow.Icon source={getAssetIDByName("ic_message_retry")} />}
+            onPress={async () => {
+                const fetch = await fetchDB()
+                if (!fetch) return showToast("Failed to reload DB", getAssetIDByName("small"))
+                return showToast("Reloaded DB", getAssetIDByName("check"))
+            }}
+        />
+    </FormSection>
+</ScrollView>)
--- plugins/UserBG/src/index.tsx
+++ plugins/UserBG/src/index.tsx
@@ -1,8 +1,8 @@
-import { findByName, findByStoreName, findByProps } from "@vendetta/metro"
-import { before, after } from "@vendetta/patcher"
+import { findByProps } from "@vendetta/metro"
+import { after } from "@vendetta/patcher"
 
-let patches = []
-
+import { data, fetchDB } from "./fetchDB"
+import Settings from "./Settings"
 type userBGData = {
     _id: string
     uid: string
@@ -10,31 +10,13 @@ type userBGData = {
     orientation: string
 }
 
-const ProfileBanner = findByName("ProfileBanner", false)
+fetchDB().then(res => res)
 
-export const onLoad = async () => {
-    const datab = await fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json").then(res => {
-        if (!res.ok) throw new Error("Failed to fetch UserBG data!")
-        return res.json()
-    })
+const unpatch = after("getUserBannerURL", findByProps("default", "getUserBannerURL"), ([user], args) => {
+    const customBanner = data?.find((i: userBGData) => i.uid === user.id) as userBGData
+    if (user && customBanner) return customBanner.img
+})
 
-    patches = [
-        before("getUserBannerURL", findByProps("default", "getUserBannerURL"), (args) => {
-            const userid = args[0]?.id
-            const unpatch = before("default", ProfileBanner, (ctx: any) => {
-                const banner = ctx[0]
-                if (!banner.bannerSource) {
-                    const customBanner = datab.find((i: userBGData) => i.uid === userid) as userBGData
-                    if (customBanner) {
-                        banner.bannerSource = { uri: customBanner.img }
-                    }
-                }
-                unpatch()
-            }, true)
-        })
-    ]
-}
+export const onUnload = () => unpatch()
 
-export const onUnload = () => {
-    for (const unpatch of patches) unpatch()
-}
+export const settings = Settings
