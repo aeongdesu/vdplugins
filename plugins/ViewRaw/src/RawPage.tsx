3e314f63a302820e27abae634142433e61fd1685
Vendicated <vendicated@riseup.net>
2023-04-17 05:12:08 +0200

    Add content block & copy button

--- plugins/ViewRaw/src/RawPage.tsx
+++ plugins/ViewRaw/src/RawPage.tsx
@@ -2,33 +2,33 @@ import { findByProps as getByProps } from "@vendetta/metro"
 import { ReactNative, constants as Constants, clipboard, React } from "@vendetta/metro/common"
 import { showToast } from "@vendetta/ui/toasts"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
+import { Codeblock } from "@vendetta/ui/components"
 import { cleanMessage } from "./cleanMessage"
-import { stylesheet } from "@vendetta/metro/common"
-import { semanticColors } from "@vendetta/ui"
 
-const { ScrollView, Text, TextInput, Platform } = ReactNative
-const { OS } = Platform
+const { ScrollView, Text } = ReactNative
 const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default as any
-
-const styles = stylesheet.createThemedStyleSheet({
-    codeBlock: {
-        fontFamily: Constants.Fonts.CODE_SEMIBOLD,
-        fontSize: 12,
-        backgroundColor: semanticColors.BACKGROUND_SECONDARY,
-        color: semanticColors.TEXT_NORMAL,
-        marginTop: 10,
-        borderRadius: 3,
-        padding: 10,
-    }
-})
 
 export default function RawPage({ message }) {
     const stringMessage = React.useMemo(() => JSON.stringify(cleanMessage(message), null, 4), [message.id])
 
+    const style = { marginBottom: 8 }
+
     return (<>
         <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
             <Button
+                style={style}
+                text="Copy Raw Content"
+                color="brand"
+                size="small"
+                disabled={!message.content}
+                onPress={() => {
+                    clipboard.setString(message.content);
+                    showToast("Copied content to clipboard", getAssetId("toast_copy_link"));
+                }}
+            />
+            <Button
                 text="Copy Raw Data"
+                style={style}
                 color="brand"
                 size="small"
                 onPress={() => {
@@ -36,16 +36,8 @@ export default function RawPage({ message }) {
                     showToast("Copied data to clipboard", getAssetId("toast_copy_link"))
                 }}
             />
-            {(OS == "ios")
-                ? <TextInput
-                    style={styles.codeBlock}
-                    onChange={() => { }}
-                    multiline
-                    value={stringMessage}
-                />
-                : <Text selectable style={styles.codeBlock}>
-                    {stringMessage}
-                </Text>}
+            {message.content && <Codeblock selectable style={style}>{message.content}</Codeblock>}
+            <Codeblock selectable>{stringMessage}</Codeblock>
         </ScrollView>
     </>)
 }
