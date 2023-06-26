39e931437a3c1dbc45f5b63cac2e7648d56f0bf6
aeongdesu <osu@kakao.com>
2023-06-26 20:06:00 +0900

    asdfasdf

--- plugins/100Accounts/index.ts
+++ plugins/100Accounts/index.ts
@@ -3,7 +3,7 @@ import { findByProps } from "@vendetta/metro"
 import { showConfirmationAlert } from "@vendetta/ui/alerts"
 
 const accounts = findByProps("MAX_ACCOUNTS")
-const DEFAULT_VALUE = 3
+const DEFAULT_VALUE = 5
 
 export const onLoad = () => {
     accounts.MAX_ACCOUNTS = 100
@@ -11,7 +11,7 @@ export const onLoad = () => {
 
 export const onUnload = () => showConfirmationAlert({
     title: "Hold on",
-    content: "Disabling plugin will *remove* accounts to 3 (default). Are you sure?",
+    content: `Disabling plugin will *remove* accounts to ${DEFAULT_VALUE} (default). Are you sure?`,
     onConfirm: () => accounts.MAX_ACCOUNTS = DEFAULT_VALUE,
     confirmText: "YEP",
     cancelText: "COPE",
