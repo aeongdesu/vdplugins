91da3db0ae597dda90294d0980ad8afa0134bb8d
aeongdesu <osu@kakao.com>
2023-06-26 20:05:00 +0900

    asdf

--- plugins/100Accounts/index.ts
+++ plugins/100Accounts/index.ts
@@ -1,22 +1,19 @@
 // thanks cyn
 import { findByProps } from "@vendetta/metro"
-import { instead } from "@vendetta/patcher"
 import { showConfirmationAlert } from "@vendetta/ui/alerts"
 
 const accounts = findByProps("MAX_ACCOUNTS")
-let unpatch: () => any
+const DEFAULT_VALUE = 3
 
 export const onLoad = () => {
     accounts.MAX_ACCOUNTS = 100
-    unpatch = instead("MAX_ACCOUNTS", accounts, () => 100)
 }
 
-export const onUnload = () => {
-    showConfirmationAlert({
-        title: "Hold on",
-        content: "Disabling plugin will *remove* accounts to 3. Are you sure?",
-        onConfirm: () => {
-            unpatch?.()
-        }
-    })
-}
+export const onUnload = () => showConfirmationAlert({
+    title: "Hold on",
+    content: "Disabling plugin will *remove* accounts to 3 (default). Are you sure?",
+    onConfirm: () => accounts.MAX_ACCOUNTS = DEFAULT_VALUE,
+    confirmText: "YEP",
+    cancelText: "COPE",
+    isDismissable: false
+})
