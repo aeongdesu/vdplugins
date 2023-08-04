7e2374f1db305e616b0e04fb248d6b962db8b30a
aeongdesu <osu@kakao.com>
2023-08-04 17:44:56 +0900

    secret

---
+++ plugins/CustomVoiceMessages/src/Constants.ts
@@ -0,0 +1,11 @@
+export type AudioMetadata = {
+    waveform: string,
+    duration: number
+}
+
+export const DRAFT_TYPE = 0
+
+export const EMPTY_DATA: AudioMetadata = {
+    waveform: "AAAAAAAAAAAA",
+    duration: 1
+}
