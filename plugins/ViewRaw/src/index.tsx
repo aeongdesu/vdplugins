343dd7e8e55a01c3ea5be4da2eaefb473790c011
aeongdesu <osu@kakao.com>
2023-09-05 08:03:11 +0900

    Update index.tsx

--- plugins/ViewRaw/src/index.tsx
+++ plugins/ViewRaw/src/index.tsx
@@ -5,7 +5,7 @@ import { React } from "@vendetta/metro/common"
 import { Forms } from "@vendetta/ui/components"
 import RawPage from "./RawPage"
 
-const ActionSheet = findByProps("openLazy", "hideActionSheet")
+const { ActionSheet } = findByProps("ActionSheet")
 const Navigation = findByProps("push", "pushLazy", "pop")
 const DiscordNavigator = findByProps("getRenderCloseButton")
 const { default: Navigator, getRenderCloseButton } = DiscordNavigator
