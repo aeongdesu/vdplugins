469159d0f6f52db937d1d4b6661a6d2ffd24b7f6
aeongdesu <osu@kakao.com>
2023-09-06 23:31:03 +0900

    userbg

--- plugins/UserBG/src/fetchDB.ts
+++
@@ -1,8 +0,0 @@
-export let data: [] | undefined
-
-export const fetchDB = async () => {
-    const datab = await fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json")
-    if (!datab.ok) return
-    data = await datab.json()
-    return data
-}
--- plugins/UserBG/src/index.tsx
+++ plugins/UserBG/src/index.tsx
@@ -1,23 +1,39 @@
+import { logger } from "@vendetta"
 import { findByProps } from "@vendetta/metro"
 import { after } from "@vendetta/patcher"
+import { safeFetch } from "@vendetta/utils"
 
-import { data, fetchDB } from "./fetchDB"
 import Settings from "./Settings"
 
-type userBGData = {
+interface userBGData {
     _id: string
     uid: string
     img: string
     orientation: string
 }
 
-fetchDB().then(res => res)
+const getUserBannerURL = findByProps("default", "getUserBannerURL")
 
-const unpatch = after("getUserBannerURL", findByProps("default", "getUserBannerURL"), ([user]) => {
-    const customBanner = data?.find((i: userBGData) => i.uid === user?.id) as userBGData
-    if (user?.banner === undefined && customBanner) return customBanner.img
-})
+let data: userBGData[]
 
-export const onUnload = () => unpatch()
+const fetchData = async () => {
+    try {
+        data = await (await safeFetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json", { cache: "no-store" })).json()
+    } catch (e) {
+        logger.error("Failed to fetch userBG data", e)
+    }
+}
+
+export default async () => {
+    await fetchData()
+    if (!data) return () => { }
+
+    const unpatch = after("getUserBannerURL", getUserBannerURL, ([user]) => {
+        const customBanner = data?.find((i: userBGData) => i.uid === user?.id)
+        if (user?.banner === undefined && customBanner) return customBanner.img
+    })
+
+    return () => unpatch()
+}
 
 export const settings = Settings
