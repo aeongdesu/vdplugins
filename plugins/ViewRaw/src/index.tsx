cac02834c69663d76b713db3a325366c4a337702
owocado <24418520+owocado@users.noreply.github.com>
2023-11-24 12:27:36 +0530

    fix ViewRaw (#24)
    i think

--- plugins/ViewRaw/src/index.tsx
+++ plugins/ViewRaw/src/index.tsx
@@ -1,15 +1,17 @@
 import { before, after } from "@vendetta/patcher"
 import { getAssetIDByName } from "@vendetta/ui/assets"
 import { findInReactTree } from "@vendetta/utils"
-import { findByProps } from "@vendetta/metro"
+import { findByName, findByProps } from "@vendetta/metro"
 import { React } from "@vendetta/metro/common"
 import { Forms } from "@vendetta/ui/components"
 import RawPage from "./RawPage"
 
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet")
 const Navigation = findByProps("push", "pushLazy", "pop")
-const DiscordNavigator = findByProps("getRenderCloseButton")
-const { default: Navigator, getRenderCloseButton } = DiscordNavigator
+const modalCloseButton =
+  findByProps("getRenderCloseButton")?.getRenderCloseButton ??
+  findByProps("getHeaderCloseButton")?.getHeaderCloseButton
+const Navigator = findByName("Navigator") ?? findByProps("Navigator")?.Navigator
 const { FormRow, FormIcon } = Forms
 
 const unpatch = before("openLazy", LazyActionSheet, ([component, key, msg]) => {
@@ -28,7 +30,7 @@ const unpatch = before("openLazy", LazyActionSheet, ([component, key, msg]) => {
                     screens={{
                         RawPage: {
                             title: "ViewRaw",
-                            headerLeft: getRenderCloseButton(() => Navigation.pop()),
+                            headerLeft: modalCloseButton?.(() => Navigation.pop()),
                             render: () => <RawPage message={message} />
                         }
                     }}
