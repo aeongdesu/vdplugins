8d81f0c87426f1654c2064e7f6be4b3965b402fa
aeongdesu <osu@kakao.com>
2023-12-05 00:10:14 +0900

    Update DeepL.ts

--- plugins/Dislate/src/api/DeepL.ts
+++ plugins/Dislate/src/api/DeepL.ts
@@ -2,7 +2,7 @@ import { DeepLResponse } from "../type"
 
 const API_URL = "https://api.deeplx.fun/translate"
 
-const translate = async (text: string, source_lang: string = "", target_lang: string, original: boolean = false) => {
+const translate = async (text: string, source_lang: string = "auto", target_lang: string, original: boolean = false) => {
     try {
         if (original) return { source_lang, text }
         const data: DeepLResponse = await (await fetch(API_URL, {
@@ -16,7 +16,7 @@ const translate = async (text: string, source_lang: string = "", target_lang: st
                 target_lang
             })
         })).json()
-        if (data.code !== 201) throw Error(`Failed to translate text from DeepL: ${data.message}`)
+        if (data.code !== 200) throw Error(`Failed to translate text from DeepL: ${data.message}`)
         return { source_lang, text: data.data }
     } catch (e) {
         throw Error(`Failed to fetch from DeepL: ${e}`)
