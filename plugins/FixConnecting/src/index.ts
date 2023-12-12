2b23b1da84d567df53f76be916fcfbd1fc36c07b
aeongdesu <osu@kakao.com>
2023-12-12 15:16:44 +0900

    FixConnecting enmity port

---
+++ plugins/FixConnecting/src/index.ts
@@ -0,0 +1,23 @@
+// https://github.com/m4fn3/FixConnecting/blob/master/src/index.tsx
+
+import { findByProps, findByStoreName } from "@vendetta/metro"
+import { FluxDispatcher } from "@vendetta/metro/common"
+import { after } from "@vendetta/patcher"
+import { getAssetIDByName } from "@vendetta/ui/assets"
+import { showToast } from "@vendetta/ui/toasts"
+
+const startSession = findByProps("startSession")
+const AuthenticationStore = findByStoreName("AuthenticationStore")
+
+const unpatch = after("startSession", startSession, () => {
+    unpatch()
+    setTimeout(() => {
+        let session_id = AuthenticationStore.getSessionId()
+        if (!session_id) {
+            FluxDispatcher?.dispatch({type: "APP_STATE_UPDATE", state: "active"})
+            showToast("Automatically fixed Connecting bug!", getAssetIDByName("Check"))
+        }
+    }, 300)
+})
+
+export const onUnload = () => unpatch()
